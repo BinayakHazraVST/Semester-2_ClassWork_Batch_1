31f0978c6b1357b60cc806559ef40d3a43e36b39
BINAYAK-HAZRA <binayak.hazra2029@vedamsot.org>
2026-02-04 15:27:05 +0530

    lecture task completed

--- Lecture_9_4Feb2025/script.js
+++ Lecture_9_4Feb2025/script.js
@@ -1,3 +1,26 @@
 document.querySelector("#grandparent").addEventListener("click", ()=>{
-    
-})
+    console.log("Grandparent Clicked!!");
+},false);
+
+
+document.querySelector("#grandparent").addEventListener("click", ()=>{
+    console.log("Grandparent Clicked!!");
+},true);
+
+document.querySelector("#parent").addEventListener("click", ()=>{
+    console.log("Parent Clicked!!");
+},false);
+
+document.querySelector("#parent").addEventListener("click", ()=>{
+    console.log("Parent Clicked!!");
+},true);
+
+document.querySelector("#child").addEventListener("click", ()=>{
+    console.log("Child Clicked!!");
+},false);
+
+
+document.querySelector("#child").addEventListener("click", ()=>{
+    console.log("Child Clicked!!");
+},true);
+
